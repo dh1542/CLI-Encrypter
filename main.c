a421cae165c40b6741bedd9c9f8a75bf8a09d3a7
dominik-arch <dominik.a.hoffmann@fau.de>
2022-04-16 16:50:02 -0400

    Put choice functionality in do while loop

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
 #define MAX_LIMIT 100
 
 int main() {
@@ -12,11 +14,37 @@ int main() {
 
     // prompt user for input
     char choice[MAX_LIMIT];
-    printf("Input 'q' to leave or 'e' to encrypt\n");
-    printf("Your choice: ");
-    fgets(choice, MAX_LIMIT, stdin);
-    printf("\n");
-    printf("%s\n", choice);
+    
+    
+
+    do
+    {   
+        char choice[MAX_LIMIT];
+        printf("Input 'q' to leave or 'e' to encrypt\n");
+        printf("Your choice: ");
+        fgets(choice, MAX_LIMIT, stdin);
+        printf("\n");
+        printf("%s\n", choice);
+
+        
+        // terminate programm --> "q" was chosen
+        if(strcmp(choice, "q") == 10){
+            printf("Goodbye! And see you soon!\n");
+            return 0;
+        }
+        // encrypt word
+        else if (strcmp(choice, "e") == 10)
+        {
+            printf("Encrypt word\n");
+            return 0;
+        }
+        else{
+            printf("Invalid input. I don't understand: %s\n", choice);
+        }
+        
+    } while (true);
+    
+    
 
     
 
