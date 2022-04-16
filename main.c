8c39c326b0fbd1dee250ff80799101bac17f2391
dominik-arch <dominik.a.hoffmann@fau.de>
2022-04-16 16:18:01 -0400

    Added user prompt for choice which currently terminates the programm regardless the input

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#define MAX_LIMIT 100
 
 int main() {
 
@@ -7,8 +8,17 @@ int main() {
     printf("##      Command-Line ENCRYPTER     ##\n");
     printf("## Copyright Dominik Hoffmann 2022 ##\n");
     printf("#####################################\n");
+    printf("\n\n\n");
 
     // prompt user for input
+    char choice[MAX_LIMIT];
+    printf("Input 'q' to leave or 'e' to encrypt\n");
+    printf("Your choice: ");
+    fgets(choice, MAX_LIMIT, stdin);
+    printf("\n");
+    printf("%s\n", choice);
+
+    
 
     
     
