f0edd7a9abb54a39945de25e8fea87c6b91abc37
dominik-arch <dominik.a.hoffmann@fau.de>
2022-04-16 16:08:24 -0400

    added greeting message

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
 int main() {
-    printf("Hello, world!\n");
+
+    // greeting message
+    printf("#####################################\n");
+    printf("##      Command-Line ENCRYPTER     ##\n");
+    printf("## Copyright Dominik Hoffmann 2022 ##\n");
+    printf("#####################################\n");
+
+    // prompt user for input
+
+    
+    
+    
+    
 }
